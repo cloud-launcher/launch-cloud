ed633d02dd1370638fc880708b5799cd1e34dcd0
Blake La Pierre <blakelapierre@gmail.com>
2015-02-26 05:54:41 -0800

    Updated DO script. Renamed test file.

--- src/providers/getData.js
+++ src/providers/getData.js
@@ -1,18 +1,21 @@
-var fs = require('fs');
-var DOWrapper = require('do-wrapper');
+import fs from 'fs';
+import DOWrapper from 'do-wrapper';
+import _ from 'lodash';
 
 var api = new DOWrapper(process.env.DO_TOKEN);
 
-promise(api.sizesGetAll.bind(api))
-  .then(
-    sizes => promise(fs.writeFile, 'sizes.json', JSON.stringify(sizes, null, '  ')),
-    error => console.log('Error retrieving sizes', error.stack));
-
-promise(api.regionsGetAll.bind(api))
-  .then(
-    regions => promise(fs.writeFile, 'regions.json', JSON.stringify(regions, null, ' ')),
-    error => console.log('Error retrieving regions', error.stack));
-
+Promise.all([
+  promise(api.sizesGetAll.bind(api))
+    .then(transformSizes)
+,
+  promise(api.regionsGetAll.bind(api))
+    .then(transformRegions)
+])
+.then(makeProfile)
+.then(
+  profile => promise(fs.writeFile, 'profile.json', JSON.stringify(profile, null, '  ')),
+  error => console.log('Error creating profile', error.stack)
+);
 
 function promise(fn, ...args) {
   return new Promise((resolve, reject) => {
@@ -21,4 +24,43 @@ function promise(fn, ...args) {
       else resolve(...rest);
     });
   });
+}
+
+function transformSizes(sizes) {
+  return new Promise((resolve, reject) => {
+    resolve(_.transform(sizes.sizes, (sizes, size) => {
+      const {slug: id, memory, vcpus, disk, transfer, price_monthly, price_hourly, regions} = size;
+      sizes[id] = {
+        id,
+        memory,
+        cpus: vcpus,
+        disk,
+        transfer,
+        price_monthly,
+        price_hourly,
+        regions
+      };
+    }, {}));
+  });
+}
+
+function transformRegions(regions) {
+  return new Promise((resolve, reject) => {
+    resolve(_.transform(_.where(regions.regions, {available: true}), (regions, region) => {
+      const {slug: id, name, sizes, available} = region;
+      regions[id] = {
+        id,
+        vicinity: name,
+        sizes
+      };
+    }, {}));
+  });
+}
+
+function makeProfile(values) {
+  const [sizes, regions] = values;
+  return {
+    sizes,
+    regions
+  };
 }
